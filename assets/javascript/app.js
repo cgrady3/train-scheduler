d6e7cfc6d9602929b2222fc782f6bcc7674ccc43
Carolyn Grady <owenv8@gmail.com>
2020-01-30 13:15:05 -0500

    pseudo code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,9 +7,16 @@ var firebaseConfig = {
     messagingSenderId: "584714869111",
     appId: "1:584714869111:web:003f5736502e31f342e972"
   };
-  
+
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
   // connect to firebase database
   var db = firebase.database();
+
+  // get user input
+  // push user input to database
+  // retrieve user input from database 
+  // do the maths
+  // write data to HTML
+  // delete inputs who's trains have come and gone
