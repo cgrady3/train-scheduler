7e357dc2f7c53eaa5a393ccb91fa4c86673a59e5
Carolyn Grady <owenv8@gmail.com>
2020-01-30 13:13:18 -0500

    added firebase and moment.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDjZGaBl1Wk7J7qcQP195dJ1BbLyMYKdRg",
+    authDomain: "train-scheduler-f3ca1.firebaseapp.com",
+    databaseURL: "https://train-scheduler-f3ca1.firebaseio.com",
+    projectId: "train-scheduler-f3ca1",
+    storageBucket: "train-scheduler-f3ca1.appspot.com",
+    messagingSenderId: "584714869111",
+    appId: "1:584714869111:web:003f5736502e31f342e972"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  // connect to firebase database
+  var db = firebase.database();
